5f45b27ecbd8a5640b85f9f92821abd877409982
lewisoh <45006322+lewisoh@users.noreply.github.com>
2018-12-03 10:18:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+import spacy
+import re
+import json
+
+	
+nlp = spacy.load('en_core_web_sm')
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
 
-if __name__ == '__main__':
+        
+@app.route("/", methods=['POST'])
+def parse_text():
+
+    input = request.get_json()
+    print(input)
+    sentence = json.dumps(input)
+    
+    
+    
+
+    doc = nlp(sentence)    
+    sentence = doc.text
+
+    for ent in doc.ents:
+        if ent.label_ == 'PERSON':
+            sentence = re.sub(ent.text, '**PII-NAME**', sentence)
+        if ent.label_ == 'ORG':
+            sentence = re.sub(ent.text, '**PII-NAME**', sentence)
+        if ent.label_ == 'NUM':
+            sentence = re.sub(ent.text, '**PII-NUM**', sentence) 
+        if ent.label_ == 'DATE':
+            sentence = re.sub(ent.text, '**PII-NUM**', sentence)
+        if ent.label_ == 'CARDINAL':
+            sentence = re.sub(ent.text, '**PII-NUM**', sentence)
+        print("test")
+        print("sentence")
+
+   
+
+    return sentence
+  
+ if __name__ == '__main__':
   app.run()
